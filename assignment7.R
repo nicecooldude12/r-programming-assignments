bb77f1185f70983f38fa7ea5f8eb1e84e61f31e2
nicecooldude12 <110140875+nicecooldude12@users.noreply.github.com>
2025-10-12 02:30:53 -0400

    Add files via upload

---
+++ assignment7.R
@@ -0,0 +1,14 @@
+data("mtcars")
+head(mtcars)
+str(mtcars)
+print(mtcars)
+
+
+s3_obj <- list(name = "Myself", age = 29, GPA = 3.5)
+class(s3_obj) <- "student_s3"
+
+setClass("student_s4",
+         slots = c(name = "character", age = "numeric", GPA = "numeric"))
+s4_obj <- new("student_s4", name = "Myself", age = 29, GPA = 3.5)
+
+print(s3_obj)
