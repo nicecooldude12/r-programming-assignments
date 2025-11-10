79020e0d8fab990a2e22ea20b0eb262809e04871
nicecooldude12 <110140875+nicecooldude12@users.noreply.github.com>
2025-11-09 21:54:00 -0500

    Add files via upload

---
+++ assignment11.R
@@ -0,0 +1,18 @@
+
+
+
+tukey_multiple <- function(x) {
+  outliers <- array(TRUE, dim = dim(x))
+  for (j in 1:ncol(x)) {
+    outliers[, j] <- outliers[, j] & tukey.outlier(x[, j])
+  }
+  outlier.vec <- vector("logical", length = nrow(x))
+  for (i in 1:nrow(x)) {
+    outlier.vec[i] <- all(outliers[i, ])
+  }
+  return(outlier.vec)
+}
+
+
+test_mat <- matrix(rnorm(50), nrow = 10)
+corrected_tukey(test_mat)
